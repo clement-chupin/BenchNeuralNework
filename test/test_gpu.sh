c6095bbc113b41aae2a02c26b26b3b3eb680ec89
Chupin Clément <chupchup2.032@gmail.com>
2022-07-26 09:29:41 +0200

    m

--- test/test_gpu.sh
+++ test/test_gpu.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=2G
+
+#SBATCH --job-name=ia_chupin
+#SBATCH --time=23:00:00
+
+#SBATCH --output=log_output.txt
+
+
+
+hostname
+sleep 10
+echo Début du job.
 python test_gpu.py
+echo Fin du job.
