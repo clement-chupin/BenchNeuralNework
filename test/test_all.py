6e4ffba117c90dc2aea315bb2703b72f0413c2c5
Chupin Clément <chupchup2.032@gmail.com>
2022-07-26 11:52:31 +0200

    m

---
+++ test/test_all.py
@@ -0,0 +1,11 @@
+import gym
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+# Parallel environments
+env = make_vec_env("Ant-v4", n_envs=4)
+
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("ppo_cartpole")
