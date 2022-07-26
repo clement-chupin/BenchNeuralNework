f710ebb4eef770c73be23bc07ffc0cc24f89abff
Chupin Clément <chupchup2.032@gmail.com>
2022-07-26 11:53:38 +0200

    m

---
+++ test/test_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=2G
+
+#SBATCH --job-name=ia_chupin
+#SBATCH --time=23:00:00
+
+#SBATCH --output=log_output.txt
+
+
+
+hostname
+sleep 10
+echo Lancement de la tache :
+python3.8 test_all.py
+echo Fin de la tache
