17334c221e693e53ea7065469ec5fd05ea4059df
Chupin Clément <chupchup2.032@gmail.com>
2022-07-28 16:47:15 +0200

    #

--- run_master_test.sh
+++ run_master_test.sh
@@ -8,7 +8,7 @@ do
     do 
         for k in {0..5}
         do
-            log_output = "./log_meso/log" + $i + "-" + $j + "-" + $k
+            log_output=("./log_meso/log" + $i + "-" + $j + "-" + $k)
             sbatch --output=$log_output single_run.sh $i $j $k 12
             #./single_run.sh $i $j $k 12
             #echo "./single_run.sh $i $j $k 12"
