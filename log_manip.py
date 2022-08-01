81cd2b3fb5eac1af84be0e377dfee06771bd1e63
Chupin Clément <chupchup2.032@gmail.com>
2022-08-01 10:32:15 +0200

    #

--- log_manip.py
+++ log_manip.py
@@ -4,7 +4,7 @@ from os import path
 
 u = Utils()
 
-
+util = u
 def get_path(policie_name,env_name,fe_k,fev_l,index=0):
     global util
     #util.all_feature_extractor[]
@@ -17,20 +17,24 @@ def get_path(policie_name,env_name,fe_k,fev_l,index=0):
     )
 
 def list_all_folder(index=0):
-    for env_i,env in enumerate(u.all_envs):
-        for po_j,policy in enumerate(u.all_policies):
-            for feature in range(len(u.all_feature_extractor)):
-                for fe_v_k,fe_v in enumerate(feature["order"]):
-                    if u.compatible_env_policie(po_j,env_i):
-                        path_log = get_path(
-                            policie_name=policy["name"],
-                            env_name=env["name"],
-                            fe_k=feature,
-                            fev_l=fe_v_k,
-                            index=index)
-                        if not path.exists(path_log):
-                            print(env["name"] + " " + policy["name"] + " " +str(feature) + " " + str(fe_v))
+    cmpt=0
+    with open("run_correct.sh", "a") as input_file:
+        for env_i,env in enumerate(u.all_envs):
+            for po_j,policy in enumerate(u.all_policies):
+                for fe_k,feature in enumerate(u.all_feature_extractor):
+                    for fe_v_k,fe_v in enumerate(feature["order"]):
+                        if u.compatible_env_policie(po_j,env_i):
+                            path_log = get_path(
+                                policie_name=policy["name"],
+                                env_name=env["name"],
+                                fe_k=fe_k,
+                                fev_l=fe_v_k,
+                                index=index)
+                            if not path.exists(path_log):
+                                cmpt=cmpt+1
+                                #print(path_log)
+                                input_file.write("\n./single_run.sh "+str(env_i) + " " +str(po_j) + " " +str(fe_k))
+    print(cmpt)
 
 
-
-list_all_folder(index=0)
+list_all_folder(index=12)
