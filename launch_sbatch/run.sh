9f52a6860248eda1169116e368a0d337c02089d8
Chupin Clément <chupchup2.032@gmail.com>
2022-08-17 09:12:47 +0200

    #

---
+++ launch_sbatch/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source activate py_conda
+source ~/IA_chupin/py_env/bin/activate
+
+
+sbatch test_gym.sh
--- launch_sbatch/test_gym.sh
+++ launch_sbatch/test_gym.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-
-
 #SBATCH --partition=normal
 #SBATCH --job-name=ia_chupin
 #SBATCH --time=1-00:00:00
@@ -9,18 +6,7 @@
 #SBATCH --output=../log_meso/log_propre_test_gym
 #SBATCH --mem-per-cpu=4G
 
-source activate py_conda
-source ~/IA_chupin/py_env/bin/activate
-
-
 
 python3.8 test_gym.sh
-
-
-
-
-
-
-
 
 
