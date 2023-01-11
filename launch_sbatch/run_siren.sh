1b0ff528d502650f07e09280d07429e6d3e296d1
chupchup <chupchup2.032@gmail.com>
2023-01-11 21:21:41 +0100

    m

---
+++ launch_sbatch/run_siren.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source activate py_conda
+source ~/IA_chupin/py_env/bin/activate
+
+for i in 0 1 6 7 8 9
+do 
+    for j in {0..5}
+    do 
+        for k in 139 140
+        do 
+            for l in 0  
+            do 
+                sbatch cpu_run.sh $i $j $k $l 11011;
+                # sleep 0.1
+            done
+        done
+    done
+done
+
+
+
+
--- utils_lib/feature_extractor_layers.py
+++ utils_lib/feature_extractor_layers.py
@@ -215,6 +215,8 @@ class FFP(nn.Linear):
         x = torch.stack([torch.sin(x),torch.cos(x)],dim=2)
         out =torch.matmul(x,self.kernel)
         return torch.flatten(out, start_dim=1)
+
+        
 class FFP11(nn.Linear):
     def __init__(self, in_features:int, order:int,device="auto"):
         self.order = order
@@ -3502,6 +3504,113 @@ class mix_all_weird_deter_07(nn.Linear):
 
 
 
+class SineLayer(nn.Module):
+
+    def __init__(self, in_features, out_features, bias=True,
+                 is_first=False, omega_0=30):
+        super().__init__()
+        self.omega_0 = omega_0
+        self.is_first = is_first
+        
+        self.in_features = in_features
+        self.linear = nn.Linear(in_features, out_features, bias=bias).to(device)
+        
+        self.init_weights()
+    
+    def init_weights(self):
+        with torch.no_grad():
+            if self.is_first:
+                self.linear.weight.uniform_(-1 / self.in_features, 
+                                             1 / self.in_features)      
+            else:
+                self.linear.weight.uniform_(-np.sqrt(6 / self.in_features) / self.omega_0, 
+                                             np.sqrt(6 / self.in_features) / self.omega_0)
+        
+    def forward(self, input):
+        return torch.sin(self.omega_0 * self.linear(input))
+    
+class Siren(nn.Module):
+    def __init__(self, in_features, hidden_features, hidden_layers, out_features, outermost_linear=False, 
+                 first_omega_0=30, hidden_omega_0=30.):
+        super().__init__()
+        
+        self.net = []
+        self.net.append(SineLayer(in_features, hidden_features, 
+                                  is_first=True, omega_0=first_omega_0))
+
+        for i in range(hidden_layers):
+            self.net.append(SineLayer(hidden_features, hidden_features, 
+                                      is_first=False, omega_0=hidden_omega_0))
+
+        if outermost_linear:
+            final_linear = nn.Linear(hidden_features, out_features)
+            
+            with torch.no_grad():
+                final_linear.weight.uniform_(-np.sqrt(6 / hidden_features) / hidden_omega_0, 
+                                              np.sqrt(6 / hidden_features) / hidden_omega_0)
+                
+            self.net.append(final_linear)
+        else:
+            self.net.append(SineLayer(hidden_features, out_features, 
+                                      is_first=False, omega_0=hidden_omega_0))
+        
+        self.net = nn.Sequential(*self.net)
+    
+    def forward(self, coords):
+        coords = coords.to(device)
+        coords = coords.clone().detach().requires_grad_(True) # allows to take derivative w.r.t. input
+        output = self.net(coords)
+        return output#, coords        
+
+    def forward_with_activations(self, coords, retain_grad=False):
+        '''Returns not only model output, but also intermediate activations.
+        Only used for visualizing activations later!'''
+        activations = OrderedDict()
+
+        activation_count = 0
+        x = coords.clone().detach().requires_grad_(True)
+        activations['input'] = x
+        for i, layer in enumerate(self.net):
+            if isinstance(layer, SineLayer):
+                x, intermed = layer.forward_with_intermediate(x)
+                
+                if retain_grad:
+                    x.retain_grad()
+                    intermed.retain_grad()
+                    
+                activations['_'.join((str(layer.__class__), "%d" % activation_count))] = intermed
+                activation_count += 1
+            else: 
+                x = layer(x)
+                
+                if retain_grad:
+                    x.retain_grad()
+                    
+            activations['_'.join((str(layer.__class__), "%d" % activation_count))] = x
+            activation_count += 1
+
+        return activations
 
 
+class siren_decompo_1(nn.Linear):
+    def __init__(self, in_features:int, order:int,device="auto"):
+        self.out_size = 64
+        self.siren = Siren(in_features=in_features, out_features=self.out_size, hidden_features=self.out_size, hidden_layers=0, outermost_linear=False).to(device)
+        super().__init__(in_features, self.out_size, bias=True)
+          
+    def get_output_size(self,):
+        return self.out_size
 
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        return self.siren(x)
+class siren_decompo_2(nn.Linear):
+    def __init__(self, in_features:int, order:int,device="auto"):
+        self.out_size = 64
+        self.siren = Siren(in_features=in_features, out_features=self.out_size, hidden_features=self.out_size, hidden_layers=0, outermost_linear=False).to(device)
+        super().__init__(in_features, self.out_size, bias=True)
+          
+    def get_output_size(self,):
+        return self.out_size
+
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        return self.siren(x)
