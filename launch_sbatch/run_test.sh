eba5157bdb2e459dffc1e844ecd74635eabf8b6d
chupchup <chupchup2.032@gmail.com>
2023-01-31 18:21:25 +0100

    m

--- launch_sbatch/run_test.sh
+++ launch_sbatch/run_test.sh
@@ -2,17 +2,17 @@ source activate py_conda
 source ~/IA_chupin/py_env/bin/activate
 
 
-    for i in 8
+for i in 16
+do 
+    for j in 2
     do 
-        for j in 3
+        for k in 167 168 169
         do 
-            for k in 172
+            for l in 0
             do 
-                for l in 0 1 2 3 4 
-                do 
-                    sbatch cpu_run.sh $i $j $k $l 11011;
-                    # sleep 0.1
-                done
+                sbatch cpu_run.sh $i $j $k $l 11011;
+                # sleep 0.1
             done
         done
     done
+done
